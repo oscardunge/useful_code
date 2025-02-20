5668a30ffbc72cc617a2701f9fec40db87450c04
Oscar Dunge <oscar@MacBook-Air-som-tillhor-AvegaIT.local>
2025-02-20 12:37:56 +0100

    transpose

--- json_parser.py
+++ json_parser.py
@@ -1,51 +1,38 @@
-import pandas as pd
 import json
+import pandas as pd
 
-def json_to_dataframe(json_file_path):
-
-
+def json_records_to_dataframe(json_records):
     try:
-        with open(json_file_path, 'r', encoding='utf-8') as f:  
-            json_data = json.load(f)
-
-        if isinstance(json_data, list):  
-            df = pd.DataFrame(json_data)
-        elif isinstance(json_data, dict):  
-            df = pd.DataFrame([json_data])  
+        df = pd.DataFrame(json_records)
+        return df.T
+    except ValueError as e:
+        if "ValueError: If using all scalar values, you must pass an index" in str(e):
+            return pd.DataFrame()
         else:
-            print("JSON data is neither a list nor a dictionary.")
-            return None
+            raise e
+    except Exception as e:
+        raise e
 
-        return df
-
-    except FileNotFoundError:
-        print(f"Error: File not found at {json_file_path}")
-        return None
-    except json.JSONDecodeError:
-        print(f"Error: Invalid JSON format in {json_file_path}")
-        return None
-    except Exception as e:  
-        print(f"An error occurred: {e}")
-        return None
-
-
-
-
-file_path = "opportunities_all.json"  
-df = json_to_dataframe(file_path)
+def read_json_file_and_transpose(file_path):
+        with open(file_path, 'r') as f:  
+            json_data = json.load(f)  
+        if isinstance(json_data, dict) and "value" in json_data: 
+            inner_data = json_data["value"]  
+            if isinstance(inner_data, list):  
+                return json_records_to_dataframe(inner_data)  
+            elif isinstance(inner_data, dict):  
+                return json_records_to_dataframe([inner_data])  
+            else:  
+                return pd.DataFrame() 
+        else:  
+            return pd.DataFrame() 
 
 
 
-if df is not None:
-    print(df.T)
-    
-    
-    
-    
-else:
-    print("Failed to load JSON into DataFrame.")
+df_from_file = read_json_file_and_transpose("data_all.json")
 
+if not df_from_file.empty:
+    print(df_from_file.T)
 
-
-
-
+df = df_from_file.T
+df.to_excel("exceldatafrom.xlsx")
