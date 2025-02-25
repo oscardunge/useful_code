1dcc4a51bb8b8f93f4b6f48380d785ea0fdb9a6e
Oscar Dunge <oscar@Mac.AVEGA.DNS>
2025-02-25 14:43:32 +0100

    auto

--- json_parser.py
+++ json_parser.py
@@ -1,12 +1,9 @@
 import json
 import pandas as pd
-# import sqlite3
 from datetime import datetime
 import psycopg2
 from sqlalchemy import create_engine
 
-
-#to config?
 
 
 def json_records_to_dataframe(json_records):
@@ -36,116 +33,95 @@ def read_json_file_and_transpose(file_path):
         return pd.DataFrame() 
 
 
-# def pandas_dataframe_to_sql():
-#     df_from_file = read_json_file_and_transpose(f"{table_name}.json")
-    
-#     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    
-#     table_name_with_timestamp = f"bronze_{table_name}_{timestamp}"
-    
-    
-#     conn = sqlite3.connect('avega_business_process_db.db')
-    
-#     df_from_file.T.to_sql(f"{table_name_with_timestamp}", conn, if_exists='replace', index=False)
-    
-#     conn.close()
-    
-#     return table_name_with_timestamp
 
-def pandas_dataframe_to_sql(table_name):
+
+def pandas_dataframe_to_sql(table_name, conn, cursor):
     df_from_file = read_json_file_and_transpose(f"{table_name}.json")
     
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     
     table_name_with_timestamp = f"{table_name}_{timestamp}"
     
-    conn = psycopg2.connect(
-        dbname='postgres',
-        user='oscar',
-        password='',
-        host='localhost',
-        port='5432'
-    )
-    
     engine = create_engine('postgresql+psycopg2://oscar:@localhost:5432/postgres')
     
-    df_from_file.T.to_sql(table_name_with_timestamp, engine, if_exists='replace', index=False, method='multi', schema="avega_dwh")
-    conn.close()
+    df_from_file.T.to_sql(table_name_with_timestamp, engine, if_exists='replace', index=False, method='multi', schema='avega_dwh')
+    # conn.close()
     
     return table_name_with_timestamp
 
-# pandas_dataframe_to_sql(table_name)
+# pandas_dataframe_to_sql('bronze_opportunities_all')
 
 
-def get_column_names(table_name, conn):
-    cursor = conn.cursor()
+def get_column_names(table_name, conn, cursor):
+    # cursor = conn.cursor()
     cursor.execute(f"SELECT column_name FROM information_schema.columns WHERE table_name = '{table_name}'")
-    columns = [row[0] for row in cursor.fetchall()]
-    cursor.close()
+    columns = [f'"{row[0]}"' for row in cursor.fetchall()]
+    # cursor.close()
     return columns
 
 
-# def main():
-#     table_name_with_timestamp = pandas_dataframe_to_sql()
-#     target_table_name = f"bronze_{table_name}"  # The table to merge into
-    
-#     conn = sqlite3.connect('avega_business_process_db.db')
-    
-#     cursor = conn.cursor()
-    
-#     # Construct the MERGE statement (adapt to your specific database syntax)
-#     merge_statement = f"""
-#     MERGE INTO {target_table_name} AS target
-#     USING {table_name_with_timestamp} AS source
-#     ON target.opportunityid = source.opportunityid;
-#     """
-    
-#     print(merge_statement)
-#     cursor.execute(merge_statement)
-#     conn.commit()
-    
-#     conn.close()
-
-# main()
 
 
-def main(table_name):
-    table_name_with_timestamp = pandas_dataframe_to_sql(table_name)  # Replace with your actual table name
-    target_table_name = 'bronze_your_table'  # The table to merge into
+def main(table_name, id):
+    error_occurred = False
     
     conn = psycopg2.connect(
-        dbname='your_database_name',
-        user='your_username',
-        password='your_password',
-        host='localhost',
-        port='5432'
+    dbname='postgres',
+    user='oscar',
+    password='',
+    host='localhost',
+    port='5432',
+    options="-c search_path=avega_dwh"
     )
     
-    columns = get_column_names(target_table_name, conn)
-    columns_str = ', '.join(columns)
-    update_str = ', '.join([f"{col} = EXCLUDED.{col}" for col in columns])
-    
-    # Construct the INSERT ... ON CONFLICT statement
-    merge_statement = f"""
-    INSERT INTO {target_table_name} ({columns_str})
-    SELECT {columns_str}
-    FROM {table_name_with_timestamp}
-    ON CONFLICT (opportunityid) DO UPDATE
-    SET {update_str};
-    """
-    
-    print(merge_statement)
     cursor = conn.cursor()
-    cursor.execute(merge_statement)
-    conn.commit()
-    
-    cursor.close()
-    conn.close()
+    try:
+        table_name_with_timestamp = pandas_dataframe_to_sql(table_name,conn, cursor)  # Replace with your actual table name
+        
+        print(table_name_with_timestamp)
+        columns = get_column_names(table_name, conn, cursor)
+        columns_str = ', '.join(columns)
+        
+        insert_statement = f"""
+        insert into {table_name} ({columns_str})
+        select {columns_str} 
+        from {table_name_with_timestamp} 
+        on conflict ({id}) do nothing 
+        ;
+        """
+        print(insert_statement)
+        
+        
+        try:
+            cursor.execute(insert_statement)
+        
+        except Exception as e:
+            print(f"Error: {e}")
+            constraint_statement = f"""
+            alter table {table_name}
+            add constraint {id} unique ({id});"""
+            
+            cursor.execute(constraint_statement)
+            cursor.execute(insert_statement)
+        # cursor.execute(f"select * from {table_name_with_timestamp}")
+        finally:
+            conn.commit()
+            error_occurred = False
+            
+    except psycopg2.Error as e:
+        print(f"Error: {e}")
+        error_occurred = True
+    finally:
+        if not error_occurred:
+            cursor.execute(f"DROP TABLE {table_name_with_timestamp}")
+            conn.commit()
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
 
 
 if __name__ == "__main__":
-    main()
-
-
-
+    # main("fff")
+    main("bronze_xxxxx_all", "xxxxxxxyid")
 
